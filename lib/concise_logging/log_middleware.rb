d9c1791ab14ade8cbce61eac1a7627296fdac1b5
kai <kai@KaiFi.local>
2015-09-18 13:46:16 +0900

    modify log pirnt

--- lib/concise_logging/log_middleware.rb
+++ lib/concise_logging/log_middleware.rb
@@ -7,9 +7,18 @@ module ConciseLogging
     def call(env)
       request = ActionDispatch::Request.new(env)
       Thread.current[:logged_ip] = request.env['HTTP_X_REAL_IP'] || request.ip
-      @app.call(env)
-    ensure
+      status, headers, response = @app.call(env)
+
+      if defined?(Rails) && headers["Content-Type"] =~ /^application\/json/ && AppInfo.first.show_json_log
+        obj = JSON.parse(response.body)
+        pretty_str = JSON.pretty_unparse(obj)
+        Rails.logger.debug("Response: " + pretty_str)
+      end
+      [status, headers, response]
+      ensure
       Thread.current[:logged_ip] = nil
     end
+
   end
+
 end
